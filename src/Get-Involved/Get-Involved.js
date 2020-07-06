9410e5ce9933886d9364b324efd3a5c1e6ef8518
Harvey Le <harveyxcle@gmail.com>
2020-07-06 11:36:57 -0400

    Changed Donate to Get-Involved, added content

--- src/Get-Involved/Get-Involved.js
+++ src/Get-Involved/Get-Involved.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Get-Involved.css'
 
 function GetInvolved() {
   return (
-    <div>
+    <div class='get-involved'>
       <h2>Get Involved</h2>
-
+      <ul>
+        <li>Get Friends to Vote</li>
+        <li>Help Register Voters</li>
+        <li>Call your Elected Officials</li>
+      </ul>
     </div>
     
   )
