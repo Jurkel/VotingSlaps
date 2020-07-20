76711779e9975d655a23dd9844cdd02eb4d37118
Harvey Le <harveyxcle@gmail.com>
2020-07-20 12:42:02 -0400

    Styling Landing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -6,17 +6,30 @@ import './Landing.css'
 function Landing() {
   return (
       <div className='LandingPage'>
-        <h2>GET INFORMED</h2>
-        <Link to='/get-informed'>
-          <button type='button' className='start-button'>
-            Start Here
-          </button>
-        </Link>
+        <div className='Landing-text'>
+          <div className='landing-image'>
+            <img src='../Resources/Voting-Illustration.png' alt='voting-illustration' />
+          </div>
+          <div className='landing-info'>
+            <h2>GET INFORMED</h2>
+            <Link to='/get-informed'>
+              <button type='button' className='start-button'>
+                Start Here
+              </button>
+            </Link>
+          </div>
+        </div>
+        
         <div className='Chart'>
           <Chart />
         </div>
+
+        <div className='movement-info'>
+          <h2>Start a movement</h2>
+          <button>Get Involved</button>
+          <img src='../Resources/Problem-solving.png' alt='problem-solving'></img>
+        </div>
       </div>
-    
   )
   
 }
