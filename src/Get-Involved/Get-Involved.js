406697799468e34a186df8d633f48d4b7ae61ba5
Harvey Le <harveyxcle@gmail.com>
2020-07-21 16:34:49 -0400

    added chart

--- src/Get-Involved/Get-Involved.js
+++ src/Get-Involved/Get-Involved.js
@@ -5,8 +5,9 @@ function GetInvolved() {
   return (
     <div className='get-involved'>
       <div className='involved-banner'></div>
+      <h2>Get Involved</h2>
       <div className='involved-container'>
-        <h2>Get Involved</h2>
+        
         <h3>Get Friends to Vote</h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         <h3>Help Register Voters</h3>
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -5,6 +5,7 @@ import './Register.css'
 function Register() {
   return (
     <div className='register-form'>
+      <div className='register-banner'></div>
       <h2>Register</h2>
       <Form src="https://qshpuqquonmzkxm.form.io/votereligibility" />
     </div>
