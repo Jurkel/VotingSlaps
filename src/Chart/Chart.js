bdf43892311c90b50b20c1917ff630b4693e475c
Harvey Le <harveyxcle@gmail.com>
2020-08-08 11:10:36 -0400

    fixed footer

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -5,57 +5,6 @@ import './Chart.css'
 class Chart extends React.Component {
   
   render() {
-    // var counts = [];
-
-    //         var newCounts = counts;
-    //         var ctx = $('#areas-of-interest-chart');
-    //         var areasOfInterestChart = new Chart(ctx, {
-    //             type: 'bar',
-    //             // responsive: true,
-    //             // maintainAspectRatio: true,
-    //             data: {
-    //                 labels: Object.keys(commentCounts),
-    //                 datasets: [{
-    //                     label: '# of Comments by Area Of Interest',
-    //                     data: counts,
-    //                     backgroundColor: colorPallete.slice(0, commentCounts.length),
-    //                     borderWidth: 1
-    //                 }]
-    //             },
-    //             options: {
-    //                 onClick: function(evt, arr) {
-    //                     // var clicked = arr[0];
-    //                     // if (clicked) {
-    //                     //     var areaOfInterestClicked = clicked._model.label;
-    //                     //     var total = commentCounts[areaOfInterestClicked];
-    //                     //     loadDashboardActions.handleAreaOfInterestEventListener(total, areaOfInterestClicked, pids);
-    //                     // }
-    //                 },
-    //                 onHover: function(e, a) {
-    //                     //$('#areas-of-interest-chart').css('cursor', a[0] ? 'pointer' : 'default');
-    //                 },
-    //                 scales: {
-    //                     xAxes: [{
-    //                         ticks: {
-    //                             callback: function(value) {
-    //                                 if (value.length > 21) {
-    //                                     return value.substr(0, 22); //truncate
-    //                                 } else {
-    //                                     return value;
-    //                                 }
-    //                             }
-    //                         }
-    //                     }],
-    //                     yAxes: [{
-    //                         ticks: {
-    //                             beginAtZero: true,
-    //                             suggestedMax: 10
-    //                         }
-    //                     }]
-    //                 }
-    //             }
-    //         });
-    //     }
     return (
       <div className='chart-container'>
         <h2>Turnout By Age</h2>
