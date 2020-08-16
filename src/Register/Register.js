28390dcee23fce385d4213af47cf2d9a4db0c54f
Harvey Le <harveyxcle@gmail.com>
2020-08-16 13:17:43 -0400

    Updated Register info

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -10,37 +10,37 @@ class Register extends React.Component {
     }
   }
 
-  // isAbleToVote = (submission) => {
-  //   let count = 0;
-  //   for(const question in submission.data) {
-  //       if(question === 'areYouAUSCitizen' && submission.data[question] === 'yes') {
-  //           count++;
-  //       } else if(question === 'areYou18YearsOldOnOrBeforeElectionDay' && submission.data[question] === 'yes') {
-  //           count++;
-  //       } else if(question === 'areYouIncarceratedInJailOrPrisonForAFelonyConviction' && submission.data[question] === 'no') {
-  //           count++;
-  //       } else if(question === 'doYouMeetYourStatesResidencyRequirement' && submission.data[question] === 'yes') {
-  //           count++;
-  //       } else if(question === 'haveYouBeenDeclaredIncompetentForVotingPurposesByAProbateCourt' && submission.data[question] === 'no') {
-  //           count++;
-  //       } else if(question === 'haveYouBeenPermanentlyDisenfranchisedForViolationsOfElectionLaws' && submission.data[question] === 'no') {
-  //           count++;
-  //       }
-  //   }
-  //   if(count === 6) {
-  //       console.log('open modal to say user is eligible to register to vote');
-  //       this.setState({
-  //         message: 'open modal to say user is eligible to register to vote'
-  //       })
-  //       return true;
-  //   } else {
-  //       console.log('open modal to say user is not eligible to register to vote');
-  //       this.setState({
-  //         message: 'open modal to say user is not eligible to register to vote'
-  //       })
-  //       return false;
-  //   }
-  // }
+  isAbleToVote = (submission) => {
+    let count = 0;
+    for(const question in submission.data) {
+        if(question === 'areYouAUSCitizen' && submission.data[question] === 'yes') {
+            count++;
+        } else if(question === 'areYou18YearsOldOnOrBeforeElectionDay' && submission.data[question] === 'yes') {
+            count++;
+        } else if(question === 'areYouIncarceratedInJailOrPrisonForAFelonyConviction' && submission.data[question] === 'no') {
+            count++;
+        } else if(question === 'doYouMeetYourStatesResidencyRequirement' && submission.data[question] === 'yes') {
+            count++;
+        } else if(question === 'haveYouBeenDeclaredIncompetentForVotingPurposesByAProbateCourt' && submission.data[question] === 'no') {
+            count++;
+        } else if(question === 'haveYouBeenPermanentlyDisenfranchisedForViolationsOfElectionLaws' && submission.data[question] === 'no') {
+            count++;
+        }
+    }
+    if(count === 6) {
+        console.log('open modal to say user is eligible to register to vote');
+        this.setState({
+          message: 'open modal to say user is eligible to register to vote'
+        })
+        return true;
+    } else {
+        console.log('open modal to say user is not eligible to register to vote');
+        this.setState({
+          message: 'open modal to say user is not eligible to register to vote'
+        })
+        return false;
+    }
+  }
   render() {
     return (
       <div className='register-form'>
