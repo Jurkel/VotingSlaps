e683731f23c27cc291b3e314e402c369af99d0f0
Harvey Le <harveyxcle@gmail.com>
2020-08-16 13:47:56 -0400

    fixed formatting on get involved

--- src/Get-Involved/Get-Involved.js
+++ src/Get-Involved/Get-Involved.js
@@ -9,7 +9,7 @@ function GetInvolved() {
       <div className='involved-container'>
         
         <h3>Get Friends to Vote</h3>
-          <p>Help encourage your friends and family to get excercise their right to vote. Have conversations about why it's important and informed people around you.</p>
+          <p>Help encourage your friends and family to excercise their right to vote. Have conversations about why it's important and informed people around you.</p>
         <h3>Help Register Voters</h3>
         <p>Be active in the community! Hold gatherings and events with others to spread the word to other young voters about the importance of making your voice heard. Your vote matters!</p>
         <h3>Call Your Elected Officials</h3>
