f5d3892c9350807af4f29a66aa776329b42a7714
Harvey Le <harveyxcle@gmail.com>
2020-08-24 09:44:02 -0400

    removed global variables

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -6,47 +6,46 @@ class Register extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: 'open modal to say user is eligible to register to vote'
     }
   }
 
-  isAbleToVote = (submission) => {
-    let count = 0;
-    for(const question in submission.data) {
-        if(question === 'areYouAUSCitizen' && submission.data[question] === 'yes') {
-            count++;
-        } else if(question === 'areYou18YearsOldOnOrBeforeElectionDay' && submission.data[question] === 'yes') {
-            count++;
-        } else if(question === 'areYouIncarceratedInJailOrPrisonForAFelonyConviction' && submission.data[question] === 'no') {
-            count++;
-        } else if(question === 'doYouMeetYourStatesResidencyRequirement' && submission.data[question] === 'yes') {
-            count++;
-        } else if(question === 'haveYouBeenDeclaredIncompetentForVotingPurposesByAProbateCourt' && submission.data[question] === 'no') {
-            count++;
-        } else if(question === 'haveYouBeenPermanentlyDisenfranchisedForViolationsOfElectionLaws' && submission.data[question] === 'no') {
-            count++;
-        }
-    }
-    if(count === 6) {
-        console.log('open modal to say user is eligible to register to vote');
-        this.setState({
-          message: 'open modal to say user is eligible to register to vote'
-        })
-        return true;
-    } else {
-        console.log('open modal to say user is not eligible to register to vote');
-        this.setState({
-          message: 'open modal to say user is not eligible to register to vote'
-        })
-        return false;
-    }
-  }
+  // isAbleToVote = (submission) => {
+  //   let count = 0;
+  //   for(const question in submission.data) {
+  //       if(question === 'areYouAUSCitizen' && submission.data[question] === 'yes') {
+  //           count++;
+  //       } else if(question === 'areYou18YearsOldOnOrBeforeElectionDay' && submission.data[question] === 'yes') {
+  //           count++;
+  //       } else if(question === 'areYouIncarceratedInJailOrPrisonForAFelonyConviction' && submission.data[question] === 'no') {
+  //           count++;
+  //       } else if(question === 'doYouMeetYourStatesResidencyRequirement' && submission.data[question] === 'yes') {
+  //           count++;
+  //       } else if(question === 'haveYouBeenDeclaredIncompetentForVotingPurposesByAProbateCourt' && submission.data[question] === 'no') {
+  //           count++;
+  //       } else if(question === 'haveYouBeenPermanentlyDisenfranchisedForViolationsOfElectionLaws' && submission.data[question] === 'no') {
+  //           count++;
+  //       }
+  //   }
+  //   if(count === 6) {
+  //       console.log('open modal to say user is eligible to register to vote');
+  //       this.setState({
+  //         message: 'open modal to say user is eligible to register to vote'
+  //       })
+  //       return true;
+  //   } else {
+  //       console.log('open modal to say user is not eligible to register to vote');
+  //       this.setState({
+  //         message: 'open modal to say user is not eligible to register to vote'
+  //       })
+  //       return false;
+  //   }
+  // }
   render() {
     return (
       <div className='register-form'>
         <div className='register-banner'></div>
         <h2>Let's see if you can vote!</h2>
-        <Form src="https://qshpuqquonmzkxm.form.io/votereligibility" onSubmit={this.isAbleToVote}/>
+        <Form src="https://qshpuqquonmzkxm.form.io/votereligibility"/>
       </div>
     )
   }
